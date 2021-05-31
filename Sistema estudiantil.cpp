beaeefe997afc0ace23b498d2f55099d21c5e9c1
Jojofs-007 <73437429+Jojofs-007@users.noreply.github.com>
2021-05-31 01:12:41 -0600

    Add files via upload

--- Sistema estudiantil.cpp
+++ Sistema estudiantil.cpp
@@ -3,44 +3,29 @@
 #include <fstream>
 
 using namespace std;
-ofstream datos_ca, datos_ci, datos_a;
+ofstream datos_a;
 
 void menu_principal();
-void retorno(int);
 
-void menu_carrera();
-void menu_ciclo();
-void menu_alumno();
-
-class alumno {
+//Clase, constructor y función de 'Carrera'
+class carrera {
 	public:
-		alumno();
-		string nombre[10];
-		char sexo[10];
-		int edad[10];
-		int contador;
-		void menu_alumno();
+		carrera();
+		string nom[3];
+		int n_ciclos[3];
+		void menu_ca();
 };
 
-alumno::alumno() {
+carrera::carrera() {
 }
 
-int main () { //terminado
-	setlocale(LC_ALL, "spanish");
-	system("color f0");
-	menu_principal();
-	return 0;
-}
-
-void menu_principal() { //terminado
+void carrera::menu_ca() {
+	system("title Sistema estudiantil: opciones para alumnos");
 	int m;
-	alumno z = alumno();
-	system ("title Sistema estudiantil: menú principal");
-	cout<<"\tMenu principal"<<endl;
-	cout<<"1. Carrera."<<endl;
-	cout<<"2. Ciclo."<<endl;
-	cout<<"3. Alumnos."<<endl;
-	cout<<"0. Salir del programa."<<endl;
+	cout<<"\n\tMenú de opciones del alumno"<<endl;
+	cout<<"1. Agregar carreras."<<endl;
+	cout<<"2. Mostrar carreras disponibles."<<endl;
+	cout<<"0. Volver al menú principal."<<endl;
 	cout<<"Digite el menú de opciones que desea utilizar: ";
 	cin>>m;
 	while((((m!=0)&&(m!=1))&&(m!=2))&&(m!=3)) {
@@ -49,31 +34,31 @@ void menu_principal() { //terminado
 		cerr<<"Digite el menú de opciones a utilizar: ";
 		cin>>m;
 	}
-	switch(m) {
-		case 0:
-			cout<<"\nGracias por usar este programa."<<endl;
-			exit(1);
-			break;
-		case 1:
-			menu_carrera();
-			break;
-		case 2:
-			menu_ciclo();
-			break;
-		case 3:
-			z.menu_alumno();
-			break;
-	}
-	menu_principal();
-}
 
 
-void menu_carrera() {
 }
+
+//Clase, constructor y función de 'Ciclo'
+void menu_ciclo();
 void menu_ciclo() {
+
 }
 
-void alumno::menu_alumno() {
+//Clase, constructor y funcion de 'Alumno' (Terminado)
+class alumno {
+	public:
+		alumno();
+		string nombre[10];
+		char sexo[10];
+		int edad[10];
+		int contador;
+		void menu_a();
+};
+
+alumno::alumno() {
+}
+
+void alumno::menu_a() {
 	int m=1;
 	while(m!=0) {
 		system("title Sistema estudiantil: opciones para alumnos");
@@ -84,7 +69,7 @@ void alumno::menu_alumno() {
 		cout<<"0. Volver al menú principal."<<endl;
 		cout<<"Digite el menú de opciones que desea utilizar: ";
 		cin>>m;
-		while((((m!=0)&&(m!=1))&&(m!=2))&&(m!=3)) {
+		while(((m!=0)&&(m!=1))&&(m!=2)) {
 			cerr<<"\aEl número digitado es inválido, intente de nuevo... ";
 			getch();
 			cerr<<"Digite el menú de opciones a utilizar: ";
@@ -114,6 +99,36 @@ void alumno::menu_alumno() {
 					}
 				} while((((sexo[i]!='m')&&(sexo[i]!='M'))&&(sexo[i]!='f'))&&(sexo[i]!='F'));
 				do {
+					cout<<"¿Desea guardar los datos del alumno en un archivo de texto?"<<endl;
+					cout<<"s/n: ";
+					cin>>x;
+					if ((x=='n')||(x=='N')) {
+						cout<<"No se guardarán los datos del alumno."<<endl;
+					} else if ((x=='s')||(x=='S')) {
+						datos_a.open("Alumnos.txt",ios::app);
+						if (datos_a.fail()) {
+							cerr<<"\aNo se pudo crear el archivo."<<endl;
+							exit(1);
+						} else {
+							datos_a<<"---------------\n";
+							datos_a<<"Nombre: "<<nombre[i]<<"\n";
+							datos_a<<"Edad: "<<edad[i]<<" años"<<"\n";
+							datos_a<<"Sexo: ";
+							if ((sexo[i]=='m')||(sexo[i]=='M')) {
+								datos_a<<"Masculino";
+							} else if ((sexo[i]=='f')||(sexo[i]=='F')) {
+								datos_a<<"Femenino";
+							}
+							datos_a<<"\n---------------\n";
+							datos_a.close();
+							cout<<"Los datos se han guardado con éxito."<<endl;
+						}
+					} else {
+						cerr<<"\aEl carácter ingresado es inválido, intente de nuevo."<<endl<<endl;
+					}
+				} while((((x!='s')&&(x!='S'))&&(x!='n'))&&(x!='N'));
+				x=0;
+				do {
 					cout<<"¿Desea agregar otro alumno?"<<endl;
 					cout<<"s/n: ";
 					cin>>x;
@@ -121,7 +136,6 @@ void alumno::menu_alumno() {
 						cout<<"Usted ingresó los datos de "<<i<<" alumnos."<<endl;
 						contador=i;
 					} else if ((x=='s')||(x=='S')) {
-						cout<<nombre[i]<<" "<<edad[i]<<endl;
 						i++;
 					} else {
 						cerr<<"\aEl carácter ingresado es inválido, intente de nuevo."<<endl<<endl;
@@ -129,23 +143,80 @@ void alumno::menu_alumno() {
 				} while((((x!='s')&&(x!='S'))&&(x!='n'))&&(x!='N'));
 			} while((x=='s')||(x=='S'));
 		} else if (m==2) {
-			system("title Sistema estudiantil: agregar alumnos");
+			system("title Sistema estudiantil: mostrar datos de alumnos");
 			cout<<"\n\tMostrar datos de alumnos"<<endl;
+			cout<<"Estos son los datos de alumnos registrados."<<endl;
 			for(int j=1; j<=contador; j++) {
 				cout<<endl<<"---------------"<<endl;
 				cout<<"Nombre: "<<nombre[j]<<endl;
 				cout<<"Edad: "<<edad[j]<<" años"<<endl;
 				cout<<"Sexo: ";
 				if ((sexo[j]=='m')||(sexo[j]=='M')) {
-					cout<<"Masculino"<<endl;
+					cout<<"Masculino";
 				} else if ((sexo[j]=='f')||(sexo[j]=='F')) {
-					cout<<"Femenino"<<endl;
+					cout<<"Femenino";
 				}
 				cout<<endl<<"---------------"<<endl;
 			}
-			cout<<"Estos son los datos de alumnos registrados."<<endl;
+			cout<<"Estos son los datos de "<<contador<<" alumnos registrados."<<endl;
 		} else if (m==0) {
 			menu_principal();
 		}
 	}
 }
+
+//int main y el menu principal (Terminado)
+int main () {
+	setlocale(LC_ALL, "spanish");
+	system("color f0");
+	menu_principal();
+	return 0;
+}
+
+void menu_principal() {
+	int m;
+	carrera x = carrera();
+	alumno z = alumno();
+	system ("title Sistema estudiantil: menú principal");
+	cout<<"\tMenu principal"<<endl;
+	cout<<"1. Carrera."<<endl;
+	cout<<"2. Ciclo."<<endl;
+	cout<<"3. Alumnos."<<endl;
+	cout<<"0. Salir del programa."<<endl;
+	cout<<"Digite el menú de opciones que desea utilizar: ";
+	cin>>m;
+	while((((m!=0)&&(m!=1))&&(m!=2))&&(m!=3)) {
+		cerr<<"\aEl número digitado es inválido, intente de nuevo... ";
+		getch();
+		cerr<<"Digite el menú de opciones a utilizar: ";
+		cin>>m;
+	}
+	switch(m) {
+		case 0:
+			cout<<"\nGracias por usar este programa."<<endl;
+			exit(1);
+			break;
+		case 1:
+			x.menu_ca();
+			break;
+		case 2:
+			menu_ciclo();
+			break;
+		case 3:
+			z.menu_a();
+			break;
+	}
+	cout<<"\nIngrese cualquier tecla para volver al menú principal... ";
+	getch();
+	system("cls");
+	menu_principal();
+}
+
+/*
+Este proyecto fue realizado el día 30/05/2021 por el grupo #1 de programación I ^u^
+Integrantes del grupo:
+	- Jonathán Josué Florián Sipaque 7490-20-3403
+	- Swany Celine Rivera Hernández 7490-20-12172
+	- Pablo Josué López de León 7490-20-24416
+	- Mauricio Rodolfo Mejía Alevon 7490-20-17072
+*/
